a2d38dcae3913df54a9d7c897c4262f69254e11d
crdavid12 <64488356+crdavid12@users.noreply.github.com>
2021-05-26 14:20:17 -0500

    prueba

---
+++ BackEndGML/GmlMapper/GmlMappers.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndGML.GmlMapper
+{
+    public class GmlMappers:Profile
+    {
+        public GmlMappers()
+        {
+            //CreateMap<Empleado, EmpleadoDto>().ReverseMap();
+        }
+    }
+}
---
+++ BackEndGML/Startup.cs
@@ -0,0 +1,64 @@
+using BackEndGML.Data;
+using BackEndGML.GmlMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndGML
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Conexion Base de datos
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConection")));
+
+            //Mappers
+            services.AddAutoMapper(typeof(GmlMappers));
+
+            //Repository
+            //services.AddScoped<IEmpleadoRepository, EmpleadoRepository>();
+
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
